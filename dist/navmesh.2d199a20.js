e24c9d3aaa28a13ac77e0014bc552e571e3dd7f9
summertides <fdfgehge@gmail.com>
2024-01-06 04:13:37 -0500

    ok

--- dist/navmesh.2d199a20.js
+++ dist/navmesh.2d199a20.js
@@ -2559,7 +2559,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63542" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63748" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- js/index.js
+++ js/index.js
@@ -1,10 +1,3 @@
-import { Application } from '@splinetool/runtime';
-
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('https://prod.spline.design/rRKMbzr1HiXTgr8N/scene.splinecode');
-
-
 var menuToggle = document.querySelector('.menu-toggle');
   
 menuToggle.addEventListener('click', function() {
